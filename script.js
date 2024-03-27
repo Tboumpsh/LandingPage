cfdd9fea7088b9a6f7a5516d3208dc2113969a46
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-03-27 16:27:33 +0330

    [create]-document for strategy
    Description:
    Create little document for header strategy.

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-// create publick menu
-function public() {}
+/** 
+**Strategy:
+Switch between different headers using a variable and case switch.
+Because of the reproducibility in other parts of the site, we can manage the header with this strategy.
+*/
+
+/*
+Creating a header for the 
+*public
+ and 
+*user 
+and 
+*admin 
+sections.
+*/
+
+// create public menu
+function public() {
+    
+}
