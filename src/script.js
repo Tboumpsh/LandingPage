4f61867ce3a36ca9496bffe9004f89b8085ce0e2
Tabasoume <145778331+Tboumpsh@users.noreply.github.com>
2024-04-05 18:37:00 +0330

    [fixed]-animation motor
    Description:
    Change the speed of movement and the amount of distance traveled for the engine.

--- src/script.js
+++ src/script.js
@@ -75,7 +75,7 @@ window.addEventListener("scroll", startAnimation);
 
 function startAnimation() {
   let animationElement = document.getElementById("animationElement");
-  let animationOffset = window.innerHeight * 2.93;
+  let animationOffset = window.innerHeight * 2.3;
 
   if (getWindowOffsetTop(animationElement) > animationOffset) {
     animationElement.style.animation = "move 18s linear";
